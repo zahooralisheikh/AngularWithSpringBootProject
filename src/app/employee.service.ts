f4ab5e4ff1a5756c60e700d7e5eec21e90bc0a14
zahooralisheikh <za257768@gmail.com>
2022-04-09 11:20:48 +0530

    created employee component

---
+++ src/app/employee.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+private baseURL = "http://localhost:8080/api/v1/employees";
+  constructor(private httpClient:HttpClient) { }
+
+  getEmployeeList():Observable<Employee[]>{
+    return this.httpClient.get<Employee[]>(`${this.baseURL}`);
+  }
+
+  createEmployee(employee: Employee): Observable<Object>{
+    return this.httpClient.post(`${this.baseURL}`,employee);
+  }
+}
