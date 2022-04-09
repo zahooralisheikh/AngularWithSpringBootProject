373586abda72ab4750d7fb2467872c8772d0be4d
zahooralisheikh <za257768@gmail.com>
2022-04-09 16:12:50 +0530

    updated component added

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -17,4 +17,13 @@ private baseURL = "http://localhost:8080/api/v1/employees";
   createEmployee(employee: Employee): Observable<Object>{
     return this.httpClient.post(`${this.baseURL}`,employee);
   }
+  getEmployeeById(id: number): Observable<Employee>{
+    return this.httpClient.get<Employee>(`${this.baseURL}/${id}`);
+  }
+  updateEmployee(id: number, employee: Employee): Observable<Object>{
+    return this.httpClient.put(`${this.baseURL}/${id}`, employee);
+  }
+  deleteEmployee(id: number): Observable<Object>{
+    return this.httpClient.delete(`${this.baseURL}/${id}`);
+  }
 }
